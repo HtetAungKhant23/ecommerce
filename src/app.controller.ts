5811607c9a43e60ac853cc1b7bd8ec7de65bb8f8
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-04-15 12:57:33 +0630

    feat(app): update vercel.json for deployment

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get()
-  handShake() {
+  handShake(): { oknasa: string } {
     return { oknasa: 'good to see you' };
   }
 }
