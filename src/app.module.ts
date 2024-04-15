d4445edf4474208d5e6db8b0d2d7816a33f48094
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-04-15 11:30:05 +0630

    feat(test): test for deployment on vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { PrismaModule } from './module/prisma/prisma.module';
 import { AuthModule } from './module/auth/auth.module';
 import { APP_FILTER } from '@nestjs/core';
 import { BadRequestExceptionFilter } from './core/filters/bad-request-exception.filter';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [PrismaModule, AuthModule],
+  controllers: [AppController],
   providers: [{ provide: APP_FILTER, useClass: BadRequestExceptionFilter }],
 })
 export class AppModule {}
